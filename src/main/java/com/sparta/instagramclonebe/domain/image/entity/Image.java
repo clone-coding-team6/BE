e710ef32f9e916e8b2f52abe803f815fa353609d
LEE <96409909+LEESEUNGRYEOL@users.noreply.github.com>
2023-03-04 00:42:16 +0900

    Feat: S3 버켓 생성 및 적용 및 Image Entity 생성 (#35)
    * Feat: 이미지 Entity, DTO 설계 및 구현 #25
    
    이미지 Entity, DTO 설계 및 구현
    
    * Feat: S3 버켓 생성 및 적용 #33
    
    S3 버켓 생성 및 적용

---
+++ src/main/java/com/sparta/instagramclonebe/domain/image/entity/Image.java
@@ -0,0 +1,44 @@
+package com.sparta.instagramclonebe.domain.image.entity;
+
+import com.sparta.instagramclonebe.domain.image.dto.ImageFileRequestDto;
+import com.sparta.instagramclonebe.domain.post.entity.Post;
+import com.sparta.instagramclonebe.global.util.Timestamped;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Image extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;                // 이미지 파일 id
+
+    @Column(nullable = false)
+    private String fileName;        // 이미지 파일명
+
+    @Column(nullable = false)
+    private String uploadPath;      // 이미지 파일 경로
+
+    @Column(nullable = false)
+    private long fileSize;          // 이미지 파일 크기
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "POST_ID", nullable = true)
+    private Post post;
+
+    public Image(String fileName, String uploadPath, long fileSize) {
+        this.fileName = fileName;
+        this.uploadPath = uploadPath;
+        this.fileSize = fileSize;
+    }
+    public Image(ImageFileRequestDto imageFileRequestDto, Post post) {
+        this.fileName = imageFileRequestDto.getFileName();
+        this.uploadPath = imageFileRequestDto.getUploadPath();
+        this.fileSize = imageFileRequestDto.getFileSize();
+        this.post = post;
+    }
+}

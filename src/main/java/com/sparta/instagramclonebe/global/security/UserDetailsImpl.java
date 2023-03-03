7fed34790c4077c3314379f7575d15cc09a5f329
Ganpyeon <jms03096@naver.com>
2023-03-03 19:15:03 +0900

    Feat: 사용자 정보(UserDtailsImpl) 설계 및 구현 #9

---
+++ src/main/java/com/sparta/instagramclonebe/global/security/UserDetailsImpl.java
@@ -0,0 +1,70 @@
+package com.sparta.instagramclonebe.global.security;
+
+import com.sparta.instagramclonebe.domain.user.entity.User;
+import com.sparta.instagramclonebe.domain.user.entity.UserRoleEnum;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails { // 사용자 정보
+
+    private final User user;
+    private final String username;
+
+    //인증이 완료된 사용자 추가하기
+    public UserDetailsImpl(User user, String username){
+        this.user = user;
+        this.username = username;
+    }
+
+    public User getUser(){
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRoleEnum role = user.getRole();
+        String authority = role.getAuthority();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+
+}

e8aadbf6dd9bdec3848a210a804099e6018540be
yshong1998 <yshong1998@gmail.com>
2023-03-03 13:39:49 +0900

    Test: 커밋 잘 들어가는지 확인

--- src/main/java/com/sparta/instagramclonebe/InstagramCloneBeApplication.java
+++ src/main/java/com/sparta/instagramclonebe/InstagramCloneBeApplication.java
@@ -2,8 +2,10 @@ package com.sparta.instagramclonebe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class InstagramCloneBeApplication {
 
     public static void main(String[] args) {

73cce6031cbbc09a3c08fafc1a9fe751abd4e1c2
Ganpyeon <jms03096@naver.com>
2023-03-03 21:38:03 +0900

    Feat: Jwt Token 검증 하는 API구현 #9

---
+++ src/main/java/com/sparta/instagramclonebe/global/jwt/JwtAuthFilter.java
@@ -0,0 +1,62 @@
+package com.sparta.instagramclonebe.global.jwt;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = jwtUtil.resolveToken(request);
+
+        // 토큰이 null 이면 다음 필터로 넘어간다
+        if (token == null) {
+            request.setAttribute("exception", HttpStatus.UNAUTHORIZED.value());
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 토큰이 유효하지 않으면 다음 필터로 넘어간다
+        if (!jwtUtil.validateToken(token)) {
+            request.setAttribute("exception", HttpStatus.UNAUTHORIZED.value());
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 유효한 토큰이라면, 토큰으로부터 사용자 정보를 가져온다.
+        Claims info = jwtUtil.getUserInfoFromToken(token);
+        try {
+            setAuthentication(info.getSubject());   // 사용자 정보로 인증 객체 만들기
+        } catch (UsernameNotFoundException e) {
+            request.setAttribute("exception", HttpStatus.UNAUTHORIZED.value());
+        }
+        // 다음 필터로 넘어간다.
+        filterChain.doFilter(request, response);
+}
+
+    private void setAuthentication(String username) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = jwtUtil.createAuthentication(username);// 인증 객체 생성
+        context.setAuthentication(authentication);
+
+        SecurityContextHolder.setContext(context);
+    }
+}

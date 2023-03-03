c1cc10aec48d2fbbfd4378cd2d4a678c7491862d
Ganpyeon <121671967+Ganpyeon@users.noreply.github.com>
2023-03-03 22:19:49 +0900

    Feat: 스프링 시큐리티 설계 및 구현 (#29)
    * setting: Spring Security dependency 추가
    
    * Feat: 스프링 시큐리티 설계 및 구현 #9

---
+++ src/main/java/com/sparta/instagramclonebe/global/config/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.sparta.instagramclonebe.global.config;
+
+
+import com.sparta.instagramclonebe.global.jwt.JwtAuthFilter;
+import com.sparta.instagramclonebe.global.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final JwtUtil jwtUtil;
+
+    // 비밀번호 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+        http.formLogin().disable();
+
+        //기본 설정인 Session 방식 사용하지 않고 JWT 방식 사용
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeRequests().antMatchers("/api/users/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
+                .anyRequest().authenticated()
+                // JWT 인증/인가를 사용하기 위한 설정
+                .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}

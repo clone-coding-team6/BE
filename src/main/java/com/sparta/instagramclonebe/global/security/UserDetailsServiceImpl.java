bc472201bf02857a9e7417703dd4de0fb7eb006d
Ganpyeon <jms03096@naver.com>
2023-03-03 19:29:31 +0900

    Feat: 인증이 완료된 사용자 불러오는 API구현 #9

---
+++ src/main/java/com/sparta/instagramclonebe/global/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.sparta.instagramclonebe.global.security;
+
+import com.sparta.instagramclonebe.domain.user.entity.User;
+import com.sparta.instagramclonebe.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+        private final UserRepository userRepository;
+
+        //인증이 완료된 사용자 불러오기
+        @Override
+        public UserDetails loadUserByUsername(String username){
+            User user = userRepository.findByUsername(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을수 없습니다."));
+
+                    return new UserDetailsImpl(user, user.getUsername());
+        }
+}

a47257db52734316f5002753864144c1e1f9e502
yshong1998 <yshong1998@gmail.com>
2023-03-05 00:11:15 +0900

    Feat: 게시글에 댓글 목록과 좋아요 숫자를 보이도록 하는 기능 추가
    추가적으로 댓글의 좋아요 숫자도 보이도록 하는 기능 추가

--- src/main/java/com/sparta/instagramclonebe/domain/like/repository/CommentLikeRepository.java
+++ src/main/java/com/sparta/instagramclonebe/domain/like/repository/CommentLikeRepository.java
@@ -1,13 +1,14 @@
 package com.sparta.instagramclonebe.domain.like.repository;
 
+import com.sparta.instagramclonebe.domain.comment.entity.Comment;
 import com.sparta.instagramclonebe.domain.like.entity.CommentLike;
-import com.sparta.instagramclonebe.domain.like.entity.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface CommentLikeRepository extends JpaRepository<CommentLike,Long> {
     Optional<CommentLike> findByCommentIdAndUserId(Long id, Long id1);
+    Long countByComment(Comment comment);
 
     void deleteByCommentIdAndUserId(Long id, Long id1);
 }

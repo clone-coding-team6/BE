206a56308ab8cc413a666153d264788f073d7dbd
이승렬 <ibban9810@icloud.com>
2023-03-04 17:46:36 +0900

    Feat: 게시글 좋아요 등록/삭제 구현 #20
    게시글 좋아요 등록 API 구현
    게시글 좋아요 삭제 API 구현

---
+++ src/main/java/com/sparta/instagramclonebe/domain/like/entity/PostLike.java
@@ -0,0 +1,43 @@
+package com.sparta.instagramclonebe.domain.like.entity;
+
+import com.sparta.instagramclonebe.domain.post.entity.Post;
+import com.sparta.instagramclonebe.domain.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class PostLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "POST_ID",nullable = false)
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID",nullable = false)
+    private User user;
+
+
+    @Builder
+    private PostLike(Post post, User user)
+    {
+        this.post =post;
+        this.user = user;
+    }
+
+    public static PostLike of(Post post, User user)
+    {
+        return PostLike.builder()
+                .post(post)
+                .user(user)
+                .build();
+    }
+}
+
---
+++ src/main/java/com/sparta/instagramclonebe/domain/like/repository/PostLikeRepository.java
@@ -0,0 +1,12 @@
+package com.sparta.instagramclonebe.domain.like.repository;
+
+import com.sparta.instagramclonebe.domain.like.entity.PostLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostLikeRepository extends JpaRepository<PostLike,Long> {
+    Optional<PostLike> findByPostIdAndUserId(Long id, Long id1);
+
+    void deleteByPostIdAndUserId(Long id, Long id1);
+}

4d84559a66bda64f678835d98747a045f2fac86f
Ganpyeon <121671967+Ganpyeon@users.noreply.github.com>
2023-03-04 16:48:10 +0900

    Fix : useremail -> userEmail 변경 , content -> contents 변경 (#39)
    * Fix: useremail -> userEmail로 변경 #1
    
    * Fix: contents -> content 변경 #6

--- src/main/java/com/sparta/instagramclonebe/domain/user/service/UserService.java
+++ src/main/java/com/sparta/instagramclonebe/domain/user/service/UserService.java
@@ -28,14 +28,14 @@ public class UserService {
 
     @Transactional
     public SuccessResponseDto<Void> signup(SignupRequestDto signupRequestDto) { // 회원 가입
-        String useremail = signupRequestDto.getUseremail();
+        String userEmail = signupRequestDto.getUserEmail();
         String password = passwordEncoder.encode(signupRequestDto.getPassword()); // 비밀번호 암호화
         String nickname = signupRequestDto.getNickname();
 
         //회원 중복 확인
-        Optional<User> found = userRepository.findByUseremail(useremail);
+        Optional<User> found = userRepository.findByUserEmail(userEmail);
         if (found.isPresent()) {
-            throw new IllegalArgumentException("중복된 아이디 입니다.");
+            throw new IllegalArgumentException("중복된 이메일 입니다.");
         }
 
         // 닉네임 중복 확인
@@ -48,7 +48,7 @@ public class UserService {
         UserRoleEnum role = UserRoleEnum.USER;
 
 
-        User user = User.of(useremail, password, role, nickname);
+        User user = User.of(userEmail, password, role, nickname);
         userRepository.save(user);
         return ResponseUtils.ok();
 
@@ -56,22 +56,22 @@ public class UserService {
 
     @Transactional(readOnly = true)
     public SuccessResponseDto<Void> login(LoginRequestDto loginRequestDto, HttpServletResponse response) { // 로그인
-        String useremail = loginRequestDto.getUseremail();
+        String userEmail = loginRequestDto.getUserEmail();
         String password = loginRequestDto.getPassword();
 
         //사용자 확인
-        if (userRepository.findByUseremail(useremail).isEmpty()) {
+        if (userRepository.findByUserEmail(userEmail).isEmpty()) {
             throw new IllegalArgumentException("등록된 사용자가 없습니다.");
         }
 
         //비밀번호 중복 확인
-        User user = userRepository.findByUseremail(useremail).get();
+        User user = userRepository.findByUserEmail(userEmail).get();
         if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
 
         // Authorization 에 token 설정
-        String token = jwtUtil.createToken(user.getUseremail(), user.getRole());
+        String token = jwtUtil.createToken(user.getUserEmail(), user.getRole());
         response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
 
         // 쿠키 설정
